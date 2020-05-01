577b3736004ea618c40b00a32b0121706cf730cb
Samra Mekonnen <smekonnen53@gmail.com>
2020-05-01 16:06:06 -0700

    edited

--- src/com/company/Rock.java
+++ src/com/company/Rock.java
@@ -21,7 +21,7 @@ public class Rock {
     }
 
     public int checkOutcome() {
-        if(cpuAnswer.equals("paper")){
+        if(cpuAnswer.equals("rock")){
             return 1;
         } else {
             return 0;
--- src/com/company/Scissors.java
+++ src/com/company/Scissors.java
@@ -21,7 +21,7 @@ public class Scissors {
     }
 
     public int checkOutcome() {
-        if(cpuAnswer.equals("paper")){
+        if(cpuAnswer.equals("scissors")){
             return 1;
         } else {
             return 0;
